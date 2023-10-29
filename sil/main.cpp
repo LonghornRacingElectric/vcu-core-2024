d48a5a2064ed039710424916e7e15897afa4801e
matt-mekha <51768075+matt-mekha@users.noreply.github.com>
2023-10-29 18:35:47 -0500

    updated for sim (#13)

--- sil/main.cpp
+++ sil/main.cpp
@@ -9,6 +9,14 @@ VcuOutput vcuOutput;
 
 VcuModel vcuModel;
 
+float arrI[11] = {0.044f, 0.0398f, 0.0792f, 0.1185f, 0.1576f, 0.1976f, 0.2356f, 0.2745f, 0.3135f, 0.3526f, 0.392f};
+float arrO[11] = {0.0044f, 0.0447f, 0.0855f, 0.1267f, 0.1688f, 0.2115f, 0.2553f, 0.2953f, 0.3363f, 0.3838f, 0.4388f};
+
+void initParameters() {
+    vcuParameters.steeringWheelToInnerWheel = CurveParameter(0.0f, 90.0f, arrI);
+    vcuParameters.steeringWheelToOuterWheel = CurveParameter(0.0f, 90.0f, arrO);
+}
+
 bool input_bool() {
     float x;
     cin >> x;
@@ -95,11 +103,12 @@ void output() {
 }
 
 int main() {
+    initParameters();
     vcuModel.setParameters(&vcuParameters);
 
     while(1) {
         input();
-        vcuModel.evaluate(&vcuInput, &vcuOutput, 0.001f);
+        vcuModel.evaluate(&vcuInput, &vcuOutput, 0.003f);
         output();
     }
 }
