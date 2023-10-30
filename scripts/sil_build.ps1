2db8d0312b4f095a32149dde06df26f3f1a6c005
matt-mekha <matthew.mekha@gmail.com>
2023-10-30 17:40:22 -0500

    working build:

--- scripts/sil_build.ps1
+++ scripts/sil_build.ps1
@@ -2,6 +2,5 @@
 $p0 = (Get-Item .).FullName
 $p1 = $p0 + "\cmake-build-debug"
 cmake -DCMAKE_BUILD_TYPE=Debug "-DCMAKE_MAKE_PROGRAM=C:/Program Files/JetBrains/CLion 2022.3.1/bin/ninja/win/ninja.exe" -G Ninja -S $p0 -B $p1
-cmake --build $p1 --target vcu_core_tests -j 16
 cmake --build $p1 --target vcu_core -j 16
 Copy-Item "./cmake-build-debug/sil/vcu_core.exe" -Destination ".."
