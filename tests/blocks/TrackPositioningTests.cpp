1e82acba7c5eb08b824bf9bc56e4f44199597a43
Dhairya Gupta <dhairyag23@gmail.com>
2024-03-20 20:07:21 -0500

    Create TrackPositioningTests.cpp

---
+++ tests/blocks/TrackPositioningTests.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Dhairya Gupta on 3/20/24.
+//
+
+#include <gtest/gtest.h>
+#include "VcuParameters.h"
+
+#include "blocks/TrackPositioning.h"
+
+TEST(TrackPositioning, TrackPosition) {
+    TrackPositioning positioning = TrackPositioning();
+
+
+}
+
