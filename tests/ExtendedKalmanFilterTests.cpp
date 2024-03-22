77ecb5eef121813b8340c25c8818003e9b507b38
matt-mekha <matthew.mekha@gmail.com>
2024-03-21 22:26:29 -0500

    test

--- tests/ExtendedKalmanFilterTests.cpp
+++ tests/ExtendedKalmanFilterTests.cpp
@@ -10,40 +10,53 @@
 #include "util/filters/ExtendedKalmanFilter.h"
 
 TEST(Kalman, ExtendedKalmanFilter) {
-    ExtendedKalmanFilter testFilter;
+  ExtendedKalmanFilter testFilter;
 
-    ControlState control = {0, 1.002003, 0};
-    VehicleState gpsEstimate = {0, 0, 0, 0, 0};
-    float delta_t = 0.003f;
+  ControlState control = {0, 1, 0};
+  VehicleState gpsEstimate = {0, 0, 0, 0, 0};
+  float delta_t = 0.003f;
 
-    // drive 10 seconds at 1m/s in the Y direction (car-centric forward)
-    for (int i = 0; i < 334; i++) {
-        testFilter.update(control, gpsEstimate, delta_t);
-    }
+  // drive 10 seconds at 1m/s in the Y direction (car-centric forward)
+  for (int i = 0; i < 3330; i++) {
+    testFilter.update(control, gpsEstimate, delta_t);
+  }
 
-    // moved a total of 50 meters
+  // moved a total of 50 meters
+  std::cout << "\nAFTER STRAIGHT\n";
+  std::cout << "X: " << testFilter.getState().x << "\n";
+  std::cout << "Y: " << testFilter.getState().y << "\n";
+  std::cout << "V_X: " << testFilter.getState().v_x << "\n";
+  std::cout << "V_Y: " << testFilter.getState().v_y << "\n";
+  std::cout << "THETA: " << testFilter.getState().theta << "\n";
 
-    std::cout << "Control A_y: " << control.a_y << "\n";
 
-        // start decelerating (3 seconds to get to 0), and turn 30 degrees clockwise each step
-        control.a_y = -3.3;
-        control.v_theta = Position::degreesToRadians(30);
+  // make right turn
+  control.a_x = -20.0;
+  control.v_theta = Position::degreesToRadians(90);
 
-        for (int i = 0; i < 100; i++) {
-            testFilter.update(control, gpsEstimate, delta_t);
-        }
+  for (int i = 0; i < 333; i++) {
+    testFilter.update(control, gpsEstimate, delta_t);
+  }
 
-        control.a_y = 1.2;
-        control.v_theta = Position::degreesToRadians(0);
+  std::cout << "\nAFTER TURN\n";
+  std::cout << "X: " << testFilter.getState().x << "\n";
+  std::cout << "Y: " << testFilter.getState().y << "\n";
+  std::cout << "V_X: " << testFilter.getState().v_x << "\n";
+  std::cout << "V_Y: " << testFilter.getState().v_y << "\n";
+  std::cout << "THETA: " << testFilter.getState().theta << "\n";
 
-        for (int i = 0; i < 333; i++) {
-            testFilter.update(control, gpsEstimate, delta_t);
-        }
+  control.a_x = 0.0;
+  control.a_y = -10.0;
+  control.v_theta = Position::degreesToRadians(0);
 
-    std::cout << "\nFINAL STATE\n";
-    std::cout << "X: " << testFilter.getState().x << "\n";
-    std::cout << "Y: " << testFilter.getState().y << "\n";
-    std::cout << "V_X: " << testFilter.getState().v_x << "\n";
-    std::cout << "V_Y: " << testFilter.getState().v_y << "\n";
-    std::cout << "THETA: " << testFilter.getState().theta << "\n";
+  for (int i = 0; i < 333; i++) {
+    testFilter.update(control, gpsEstimate, delta_t);
+  }
+
+  std::cout << "\nAFTER BRAKING\n";
+  std::cout << "X: " << testFilter.getState().x << "\n";
+  std::cout << "Y: " << testFilter.getState().y << "\n";
+  std::cout << "V_X: " << testFilter.getState().v_x << "\n";
+  std::cout << "V_Y: " << testFilter.getState().v_y << "\n";
+  std::cout << "THETA: " << testFilter.getState().theta << "\n";
 }
