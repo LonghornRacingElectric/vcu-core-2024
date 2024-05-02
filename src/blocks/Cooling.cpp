cc00bee273b8284336eea8135b7dc0a2bf4dd324
matt-mekha <matthew.mekha@gmail.com>
2024-05-02 14:16:14 -0400

    endurance take two

--- src/blocks/Cooling.cpp
+++ src/blocks/Cooling.cpp
@@ -5,7 +5,7 @@ void Cooling::setParameters(VcuParameters *params) {
 }
 
 void Cooling::evaluate(VcuParameters *params, CoolingInput *input, CoolingOutput *output, float deltaTime) {
-    output->pumpOutput = 0.0f;// TODO 1.0f;
+    output->pumpOutput = 1.0f;
     output->radiatorOutput = params->coolingRadiatorFanCurve(input->motorTemp);
     output->batteryFansOutput = params->coolingBatteryFanCurve(input->batteryTemp);
 }
