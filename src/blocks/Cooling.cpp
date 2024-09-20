a8da8571160386195d43052433bce7a41163b6a0
matt-mekha <matthew.mekha@gmail.com>
2024-09-20 05:52:20 -0500

    cooling fix

--- src/blocks/Cooling.cpp
+++ src/blocks/Cooling.cpp
@@ -8,7 +8,7 @@ void Cooling::evaluate(VcuParameters *params, CoolingInput *input, CoolingOutput
   if(input->parkOrDrive) {
     output->pumpOutput = 1.0f;
     output->radiatorOutput = 0.3f;//params->coolingRadiatorFanCurve(input->motorTemp);
-    output->batteryFansOutput = params->coolingBatteryFanCurve(input->batteryTemp);
+    output->batteryFansOutput = 1.0f;//params->coolingBatteryFanCurve(input->batteryTemp);
   } else {
     output->pumpOutput = 0.0f;
     output->radiatorOutput = 0.0f;
